ba28e76a075171802ef82c36acc932993589923f
Pavel Kouřil <pk@pavelkouril.cz>
2019-10-05 15:15:35 +0200

    spawning test AI unit with wandering behavior

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : Singleton<GameManager>
 {
     public Level CurrentLevel;
 
+    private Text _debugText;
+
+    private AiControllerUnit _unit;
+
     private void Start()
     {
         CurrentLevel = new Level();
@@ -21,7 +25,38 @@ public class GameManager : Singleton<GameManager>
             tmp += "\r\n";
         }
         var textGo = GameObject.Find("DebugText");
-        var text = textGo.GetComponent<Text>();
-        text.text = tmp;
+        _debugText = textGo.GetComponent<Text>();
+
+
+        if (CurrentLevel.TryGetAnyWalkablePosition(out Vector2Int posToSpawnAiUnit))
+        {
+            Debug.Log($"spawning at {posToSpawnAiUnit}");
+
+            var unitGo = new GameObject("unit");
+            _unit = unitGo.AddComponent<AiControllerUnit>();
+            _unit.CurrentPosition = posToSpawnAiUnit;
+            _unit.CurrentDirection = Vector2Int.up; 
+        }
+    }
+
+    private void Update()
+    {
+        string tmp = "";
+        for (int i = 0; i < CurrentLevel.Size; i++)
+        {
+            for (int j = 0; j < CurrentLevel.Size; j++)
+            {
+                if (_unit.CurrentPosition == new Vector2Int(i, j))
+                {
+                    tmp += '@';
+                }
+                else
+                {
+                    tmp += (char)CurrentLevel.Map[i, j];
+                }
+            }
+            tmp += "\r\n";
+        }
+        _debugText.text = tmp;
     }
 }
