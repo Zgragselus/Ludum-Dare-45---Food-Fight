86f21d25ce3eabc075eeefca087e7289692936fd
Zgragselus <vilem.otte@post.cz>
2019-10-05 14:15:19 +0200

    Initial corridors

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -8,17 +8,24 @@ public enum CellType : byte
 {
     EMPTY = (byte)' ',
     WALL = (byte)'#',
-    FLOOR = (byte)'.'
+    FLOOR = (byte)'.',
+    CORRIDOR = (byte)'|'
 }
 
 public class BSPNode
 {
     public int split;
+    public int axis;
 
     public bool isLeaf;
 
+    public bool isConnected;
+
     public Vector2Int min;
     public Vector2Int max;
+
+    public Vector2Int realMin;
+    public Vector2Int realMax;
 
     public BSPNode[] children;
 
@@ -27,6 +34,8 @@ public class BSPNode
         children = new BSPNode[2];
         children[0] = null;
         children[1] = null;
+
+        isConnected = false;
     }
 }
 
@@ -38,6 +47,125 @@ public class Level : MonoBehaviour, ILevel
     int maxDepth;
 
     public Text text;
+
+    void BuildCorridor(Vector2Int entrance, Vector2Int exit, int axis)
+    {
+        Vector2Int distance = new Vector2Int(exit.x - entrance.x, exit.y - entrance.y);
+
+        Vector2Int[] waypoint = new Vector2Int[4];
+        waypoint[0] = new Vector2Int(entrance.x, entrance.y);
+        if (axis == 0)
+        {
+            waypoint[1] = new Vector2Int(entrance.x + distance.x / 2, entrance.y);
+            waypoint[2] = new Vector2Int(entrance.x + distance.x / 2, exit.y);
+        }
+        else
+        {
+            waypoint[1] = new Vector2Int(entrance.x, entrance.y + distance.y / 2);
+            waypoint[2] = new Vector2Int(exit.x, entrance.y + distance.y / 2);
+        }
+        waypoint[3] = new Vector2Int(exit.x, exit.y);
+
+        int x = entrance.x;
+        int y = entrance.y;
+        int current = 1;
+        while (current != 4)
+        {
+            while (!(x == waypoint[current].x && y == waypoint[current].y))
+            {
+                if (x < waypoint[current].x)
+                {
+                    x++;
+                }
+                else if (x > waypoint[current].x)
+                {
+                    x--;
+                }
+                else if (y < waypoint[current].y)
+                {
+                    y++;
+                }
+                else if (y > waypoint[current].y)
+                {
+                    y--;
+                }
+
+                map[x][y] = CellType.CORRIDOR;
+            }
+             current++;
+        }
+    }
+
+    void RecursiveConnect(BSPNode node)
+    {
+        if (node.isLeaf)
+        {
+            node.isConnected = true;
+        }
+        else
+        {
+            if (!node.children[0].isConnected)
+            {
+                RecursiveConnect(node.children[0]);
+            }
+
+            if (!node.children[1].isConnected)
+            { 
+                RecursiveConnect(node.children[1]);
+            }
+
+            if (node.children[0].isLeaf && node.children[1].isLeaf)
+            {
+                if (node.axis == 0)
+                {
+                    int begin = node.children[0].realMin.y + 1;
+                    int end = node.children[0].realMax.y - 1;
+                    int position = UnityEngine.Random.Range(begin, end + 1);
+
+                    Vector2Int entrance = new Vector2Int(node.children[0].realMax.x - 1, position);
+
+                    map[entrance.x][entrance.y] = CellType.CORRIDOR;
+                    
+                    begin = node.children[1].realMin.y + 1;
+                    end = node.children[1].realMax.y - 1;
+                    position = UnityEngine.Random.Range(begin, end + 1);
+
+                    Vector2Int exit = new Vector2Int(node.children[1].realMin.x, position);
+
+                    map[exit.x][exit.y] = CellType.CORRIDOR;
+
+                    BuildCorridor(entrance, exit, node.axis);
+                }
+                else
+                {
+                    int begin = node.children[0].realMin.x + 1;
+                    int end = node.children[0].realMax.x - 1;
+                    int position = UnityEngine.Random.Range(begin, end + 1);
+
+                    Vector2Int entrance = new Vector2Int(position, node.children[0].realMax.y - 1);
+
+                    map[entrance.x][entrance.y] = CellType.CORRIDOR;
+
+                    begin = node.children[1].realMin.x + 1;
+                    end = node.children[1].realMax.x - 1;
+                    position = UnityEngine.Random.Range(begin, end + 1);
+
+                    Vector2Int exit = new Vector2Int(position, node.children[1].realMin.y);
+
+                    map[exit.x][exit.y] = CellType.CORRIDOR;
+
+                    BuildCorridor(entrance, exit, node.axis);
+                }
+            }
+            else
+            {
+
+            }
+
+            node.isConnected = true;
+        }
+
+    }
 
     void RecursiveFill(BSPNode node)
     {
@@ -66,14 +194,14 @@ public class Level : MonoBehaviour, ILevel
             int shrinkDown = shrinkVertical / 2;
             int shrinkUp = shrinkVertical / 2 + shrinkVertical % 2;
 
-            Vector2Int min = new Vector2Int(node.min.x + shrinkLeft, node.min.y + shrinkDown);
-            Vector2Int max = new Vector2Int(node.max.x - shrinkRight, node.max.y - shrinkUp);
+            node.realMin = new Vector2Int(node.min.x + shrinkLeft, node.min.y + shrinkDown);
+            node.realMax = new Vector2Int(node.max.x - shrinkRight, node.max.y - shrinkUp);
 
-            for (int i = min.x; i < max.x; i++)
+            for (int i = node.realMin.x; i < node.realMax.x; i++)
             {
-                for (int j = min.y; j < max.y; j++)
+                for (int j = node.realMin.y; j < node.realMax.y; j++)
                 {
-                    if (i == min.x || j == min.y || i == (max.x - 1) || j == (max.y - 1))
+                    if (i == node.realMin.x || j == node.realMin.y || i == (node.realMax.x - 1) || j == (node.realMax.y - 1))
                     {
                         map[i][j] = CellType.WALL;
                     }
@@ -130,6 +258,8 @@ public class Level : MonoBehaviour, ILevel
                 Vector2Int rightMax = new Vector2Int(max.x, max.y);
 
                 n.children[1] = RecursiveBuild(rightMin, rightMax, depth + 1);
+
+                n.axis = 0;
             }
             else
             {
@@ -146,6 +276,8 @@ public class Level : MonoBehaviour, ILevel
                 Vector2Int rightMax = new Vector2Int(max.x, max.y);
 
                 n.children[1] = RecursiveBuild(rightMin, rightMax, depth + 1);
+
+                n.axis = 1;
             }
 
             return n;
@@ -191,6 +323,8 @@ public class Level : MonoBehaviour, ILevel
         BSPNode root = RecursiveBuild(new Vector2Int(0, 0), new Vector2Int(size, size), 0);
 
         RecursiveFill(root);
+
+        RecursiveConnect(root);
     }
 
     // Start is called before the first frame update
